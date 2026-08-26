e050303b66ac210feda4ed409434c19800422910
Abdulrahman Hajjar <316491264+rahman-997@users.noreply.github.com>
2026-08-26 04:43:46 -0700

    Make sitemap metadata static-export compatible

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 const baseUrl = "https://abdulrahman-hajjar-dev.netlify.app";
 
 export default function sitemap(): MetadataRoute.Sitemap {
