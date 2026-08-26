20795f0db1d255f740ea039eef6788713ee5e244
Abdulrahman Hajar <316491264+rahman-997@users.noreply.github.com>
2026-08-26 06:42:41 -0700

    Make Twitter preview static-export compatible

--- app/twitter-image.tsx
+++ app/twitter-image.tsx
@@ -1,4 +1,5 @@
 import OpenGraphImage, { alt, contentType, size } from "./opengraph-image";
 
+export const dynamic = "force-static";
 export { alt, contentType, size };
 export default OpenGraphImage;
